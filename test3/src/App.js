fdb32fa23758cabd44baaba48b396d1a846f881a
Vaidupael <100674522+Vaidupatel@users.noreply.github.com>
2024-01-18 21:05:19 +0530

    Created new inotebook app for full MERN stack, News app work panding

--- test3/src/App.js
+++ test3/src/App.js
@@ -4,45 +4,112 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 import { Route, Routes } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
 export default class App extends Component {
   pageSize = 12;
+  state = {
+    progress: 0,
+  };
+  setProgress(progress) {
+    this.setState({ progress: progress });
+  }
   render() {
     return (
       <>
         <Navbar />
-        {/* <News pageSize={this.pageSize} country={"in"} category={"general"} /> */}
+        <LoadingBar color="#f11946" progress={this.state.progress} />
+        {/* <News setProgress = {this.setProgress} pageSize={this.pageSize} country={"in"} category={"general"} /> */}
         <Routes>
-          <Route exact
+          <Route
+            exact
             path="/"
-            element={<News key="general" pageSize={this.pageSize} country={"in"} category={"general"} />}
-          />
-          <Route exact
-            path="/business"
-            element={<News key="Business" pageSize={this.pageSize} country={"in"} category={"Business"} />}
-          />
-          <Route exact
-            path="/entertainment"
             element={
-              <News key="Entertainment" pageSize={this.pageSize} country={"in"} category={"Entertainment"} />
+              <News
+                setProgress={this.setProgress}
+                key="general"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"general"}
+              />
             }
           />
-          <Route exact
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={this.setProgress}
+                key="Business"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Business"}
+              />
+            }
+          />
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={this.setProgress}
+                key="Entertainment"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Entertainment"}
+              />
+            }
+          />
+          <Route
+            exact
             path="/health"
-            element={<News key="Health" pageSize={this.pageSize} country={"in"} category={"Health"} />}
+            element={
+              <News
+                setProgress={this.setProgress}
+                key="Health"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Health"}
+              />
+            }
           />
-          <Route exact
+          <Route
+            exact
             path="/science"
-            element={<News key="Science" pageSize={this.pageSize} country={"in"} category={"Science"} />}
+            element={
+              <News
+                setProgress={this.setProgress}
+                key="Science"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Science"}
+              />
+            }
           />
-          <Route exact
+          <Route
+            exact
             path="/sports"
-            element={<News key="Sports" pageSize={this.pageSize} country={"in"} category={"Sports"} />}
+            element={
+              <News
+                setProgress={this.setProgress}
+                key="Sports"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Sports"}
+              />
+            }
           />
-          <Route exact
+          <Route
+            exact
             path="/technology"
             element={
-              <News key="Technology" pageSize={this.pageSize} country={"in"} category={"Technology"} />
+              <News
+                setProgress={this.setProgress}
+                key="Technology"
+                pageSize={this.pageSize}
+                country={"in"}
+                category={"Technology"}
+              />
             }
           />
         </Routes>
--- test3/src/components/News.js
+++ test3/src/components/News.js
@@ -31,6 +31,7 @@ export class News extends Component {
     )} - NewsMonkey`;
   }
   async updateNews() {
+    this.props.setProgress(0);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a38ceee388b24d6b891eb7196d16e0ea&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
@@ -40,6 +41,7 @@ export class News extends Component {
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
   async componentDidMount() {
     this.updateNews();
